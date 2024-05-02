600a25dfb4492f7ded8ba3ebec30d8b386e11c8d
Xavier Hampton <xaviermichaelhampton@gmail.com>
2024-05-02 13:34:32 -0500

    Finished menu page with pictures/text

--- src/menu.js
+++ src/menu.js
@@ -35,7 +35,7 @@ class menu {
     static build(content) {
         const container = this.createElement('div','','container')
         const page = this.createElement('div','','page')
-        page.appendChild(this.createElement('h1','~ The Menu ~', 'title'))
+        page.appendChild(this.createElement('h1','~ Entrees ~', 'title'))
         page.appendChild(this.makeItem('En Passant Platter', 'A regal assortment of grilled meats including succulent steak medallions, herb-roasted chicken, and garlic butter shrimp, served with roasted vegetables and garlic mashed potatoes. ', menu1))
         page.appendChild(this.makeItem("Bishop's Baked Brie", 'Warm, gooey Brie cheese enveloped in flaky puff pastry, topped with a sweet fig jam and toasted almonds. A decadent indulgence fit for the most astute strategist.', menu2))
         page.appendChild(this.makeItem("Rook's Risotto Royale", "Creamy Arborio rice simmered in a rich mushroom and truffle broth, crowned with shavings of Parmesan cheese. A hearty dish worthy of the rook's steadfast defense.", menu3))
